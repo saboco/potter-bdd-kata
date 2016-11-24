252e9c5231ffbfd9f1ff7ea468ebe982dd96e6a3
saboco <sabotero@gmail.com>
2016-11-24 17:43:06 +0100

    Add Domain and Domain.Tests

---
+++ Domain/Book.cs
@@ -0,0 +1,28 @@
+﻿namespace Domain
+{
+    public class Book
+    {
+        public decimal Price { get; private set; }
+        public object VolumeNumber { get; set; }
+
+        public Book()
+        {
+            Price = 8.0m;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var book = obj as Book;
+            if (book != null == false)
+            {
+                return base.Equals(obj);
+            }
+            return book.VolumeNumber == VolumeNumber;
+        }
+
+        public override int GetHashCode()
+        {
+            return VolumeNumber.GetHashCode();
+        }
+    }
+}
